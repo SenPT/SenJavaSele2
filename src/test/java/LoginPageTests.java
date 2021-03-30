fac62853d826d79a7c700d6ac7d2b252c89a0c3a
sen.phan <phanthisen51195@gmail.com>
2021-03-30 17:39:42 +0700

    initial commit

---
+++ src/test/java/LoginPageTests.java
@@ -0,0 +1,25 @@
+import com.google.common.annotations.VisibleForTesting;
+import driver.DriverManager;
+import driver.DriverManagerFactory;
+import driver.DriverType;
+import objects.LoginPage;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPageTests {
+    DriverManager driverManager;
+    WebDriver driver;
+    LoginPage loginPage;
+
+    @BeforeClass
+    public void setUp(){
+        driverManager = DriverManagerFactory.getDriverManager((DriverType.CHROME));
+        driver.get("");
+    }
+
+    @Test
+    public void loginTest(){
+        loginPage = new LoginPage(driver);
+        loginPage.login("","");
+        assertEquals(loginPage.getLoginErrorMessage(),"");
+    }
+}
